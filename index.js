3dfcc172d88aace9edb5f1db7dd38a81f00b94b0
Soniya Jaiswal <soniyajaiswal995@gmail.com>
2023-11-11 16:14:00 +0530

    port

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const mysql2=require('mysql2');
 const cors = require('cors')
 const upload = require('./middleware.js')
 const path = require('path'); 
-const fs = require('fs');    
-
+const fs = require('fs');  
+  
+require('dotenv').config();
 
 const userRoutes = require('./routes/userRoutes.js');
 const postRoutes = require('./routes/postRoutes.js')
@@ -17,6 +18,6 @@ app.use(express.json())
 app.use('/user',userRoutes);
 app.use('/posts', postRoutes);
 
-app.listen(3001,()=>{
+app.listen(process.env.PORT,()=>{
     console.log("server on ")
 })
